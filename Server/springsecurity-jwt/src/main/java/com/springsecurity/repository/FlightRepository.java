558b9b362e602de58e249584dc758dbc6fb512bb
priyadharshini <priyaram2021@gmail.com>
2023-10-03 11:18:37 +0530

    crud operation

---
+++ Server/springsecurity-jwt/src/main/java/com/springsecurity/repository/FlightRepository.java
@@ -0,0 +1,13 @@
+package com.springsecurity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springsecurity.entity.Flight;
+
+@Repository
+public interface FlightRepository extends JpaRepository<Flight, Integer> {
+
+	Flight findByFlightName(String flightName);
+
+}
--- Server/src/main/java/com/airline/ticketbooking/exceptions/TripNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.airline.ticketbooking.exceptions;
-
-@SuppressWarnings("serial")
-public class TripNotFoundException extends RuntimeException {
-	public TripNotFoundException(String exception) {
-		super(exception);
-	}
-
-}
