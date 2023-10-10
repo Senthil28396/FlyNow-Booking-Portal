35c23dcf347df53dd0fa2ad5daf11c417377854c
mukil <mukila.kanibalan@frenzo.io>
2023-10-10 18:24:57 +0530

    dashboard bug fixed

--- Client/src/pages/admin/flights/page.jsx
+++ Client/src/pages/admin/flights/page.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import { getAll } from "../../../api/flights/fetchers";
-import useQuery from "../../../hooks/useQuery";
+import DashboardHeader from "../../../components/dashboardHeader/Dashboard";
 const AdminDashboardFlights = () => {
-  const { data, error } = useQuery(getAll);
-  console.log({ err: error?.message });
-  return <div>AdminDashboardFlights</div>;
+  const [flights, setFlights] = useState([]);
+  useEffect(() => {
+    getAll().then(response => {
+      console.log({ response });
+      setFlights(() => [...response]);
+    });
+  }, []);
+  return (
+    <div>
+      <DashboardHeader
+        sectionClass="flex justify-between p-4"
+        buttonLabel="add flight"
+        to="/admin/flights/add"
+        title="list of Flights"
+        buttonClass="bg-indigo-600 text-white px-8 py-2 rounded-sm"
+      />
+    </div>
+  );
 };
 
 export default AdminDashboardFlights;
