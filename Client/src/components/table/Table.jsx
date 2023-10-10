9d32964b746cbf53d083eb19d288bbbf7268d77d
mukil <mukila.kanibalan@frenzo.io>
2023-10-10 19:51:34 +0530

    table implemented

---
+++ Client/src/components/table/Table.jsx
@@ -0,0 +1,39 @@
+import { capitalize } from "lodash";
+
+const Row = ({ data }) => {
+  const columnCount = Object.keys(data[0]).length;
+  const headerColumns = Object.keys(data[0]);
+  console.log({ columnCount, headerColumns });
+  return (
+    <table className="block w-full px-3">
+      <thead className="block  w-full text-left">
+        <tr className="flex items-center  px-2 mb-1">
+          {headerColumns.map((val, index) => (
+            <th
+              key={index}
+              className="flex-1 text-ellipsis whitespace-nowrap overflow-hidden"
+            >
+              {capitalize(val)}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody className="block w-full text-left">
+        {data.map((row, index) => (
+          <tr
+            key={index}
+            className="flex items-center px-2 my-2 shadow-sm even:bg-slate-300 odd:bg-indigo-300 py-2"
+          >
+            {headerColumns.map(column => (
+              <th className="flex-1 font-normal text-ellipsis whitespace-nowrap overflow-hidden">
+                {capitalize(row[column])}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Row;
