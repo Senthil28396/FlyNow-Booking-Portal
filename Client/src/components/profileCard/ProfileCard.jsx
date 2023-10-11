a671930d9a8ad99e5ac9cdd7fecdd6a6697cffa2
priyadharshini <priyaram2021@gmail.com>
2023-10-11 20:20:19 +0530

    getting profile by id

--- Client/src/components/profileCard/ProfileCard.jsx
+++ Client/src/components/profileCard/ProfileCard.jsx
@@ -1,9 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContextProvider";
-
+import { useEffect, useState } from "react";
+import { getOne } from "../../api/passengers/fetchers";
+import _ from "lodash";
 const ProfileCard = () => {
   const { logout } = useAuth();
   const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    try {
+      const id = Number(localStorage.getItem("currentUserId"));
+      getOne(id).then((response) => {
+        const updatedResponse = _.omit(response, []);
+        setUser({ ...updatedResponse });
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  }, []);
+  console.log({ user });
   const handleLogout = () => {
     try {
       logout();
